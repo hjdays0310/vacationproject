6efc20f329b9edbaa6fac3af07d9b8402c5b7f71
hjdays0310 <44605442+hjdays0310@users.noreply.github.com>
2019-02-06 22:45:42 +0900

    Update user.php

--- user.php
+++ user.php
@@ -6,29 +6,6 @@ $gender = $_POST['gender'];
 
 $conn = mysqli_connect("localhost","root","koa991002","test");
 
-
-
-if($_POST[$name]==""){
-$message = "이름을 입력하세요";
-echo "<script type='text/javascript'>alert('$message');</script>";
- echo("<script>location.href='user.html';</script>");
-};
-if($_POST[$id]==""){
-$message1 = "아이디를 입력하세요";
-echo "<script type='text/javascript'>alert('$message1');</script>";
- echo("<script>location.href='user.html';</script>");
-};
-if($_POST[$pw]==""){
-$message = "비빌먼호를 입력하세요";
-echo "<script type='text/javascript'>alert('$message');</script>";
- echo("<script>location.href='user.html';</script>");
-};
-if($_POST[$gender]==""){
-$message = "성별을 입력하세요";
-echo "<script type='text/javascript'>alert('$message');</script>";
- echo("<script>location.href='user.html';</script>");
-};
-
 if ($conn) {
 $sel = "select * from user_info where id = '$id'";
 $sql = mysqli_query($conn,$sel);
